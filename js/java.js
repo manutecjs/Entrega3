92897336bc1a8ab8fe8c4a7db5388d16f9aa6469
Manuel Gómez Pose <gomezposem@gmail.com>
2024-05-25 16:52:21 -0300

    1er commit

---
+++ js/java.js
@@ -0,0 +1,34 @@
+// const el = document.getElementById("poster")
+// const height = el.clientHeight
+// const width = el.clientWidth
+
+// el.addEventListener("mousemove", (evt) => {
+//     const {layerX, layerY} = evt
+
+//     const yRotation = (
+//         (layerX - width / 2) / width
+//     ) * 20
+
+//     const xRotation = (
+//         (layerY - height / 2) / width
+//     ) * 20
+
+//     const string = `
+//     perspective(500px)
+//     scale(1.1)
+//     rotateX(${xRotation}deg)
+//     rotateY(${yRotation}deg)
+//     `
+
+//     el.style.transform = string
+// })
+
+// el.addEventListener("mouseout", () => {
+
+//     el.style.transform = `
+//     perspective(500px)
+//     scale(1)
+//     rotateX(0)
+//     `
+
+})
